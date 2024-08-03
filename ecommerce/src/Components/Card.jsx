fe3a435f268093501a22faf74c182e35536ad403
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-08-03 23:32:21 +0530

    Update Card.jsx

--- ecommerce/src/Components/Card.jsx
+++ ecommerce/src/Components/Card.jsx
@@ -18,6 +18,9 @@ export default function Card({ title, price, src }) {
             <p className="mb-3 font-normal text-gray-700 dark:text-black">
               {price}
             </p>
+            <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <button>Add to Cart</button>
+          </div>
           </div>
         </div>
       </div>
